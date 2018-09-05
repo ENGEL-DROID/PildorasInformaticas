1c293ec76efa11565b142f8707d66f249eca0133
LuisCarlos <ingluiscarlos02@gmail.com>
2018-09-04 22:28:34 -0500

    herencia y polimorfismo

---
+++ src/herencia_y_polimorfismo/Jefe.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia_y_polimorfismo;
+
+/**
+ *
+ * @author Antonella
+ */
+public class Jefe extends Empleado{
+  
+    public Jefe(String nom, double sue, int agno, int mes, int dia) {
+        super(nom,  sue,  agno,  mes,  dia);
+    }
+   
+    public void EstableceIncentivo(double b){
+    incentivo=b;
+}
+    
+    @Override
+    public double getdameSueldo(){
+        double SueldoJefe=super.getdameSueldo();
+        return SueldoJefe + incentivo;
+    }
+    
+  private double incentivo;
+}
---
+++ src/herencia_y_polimorfismo/UsoEmpleado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia_y_polimorfismo;
+
+/**
+ *
+ * @author Antonella
+ */
+public class UsoEmpleado {
+    public static void main(String[] arg){
+     Jefe JefeRecursosHumanos=new Jefe("luis",  55000, 2006, 9,25);
+     JefeRecursosHumanos.EstableceIncentivo(2570);
+     
+     Empleado[] misEmpleados=new Empleado[4];
+     misEmpleados[0]=new Empleado("antonella", 30000, 2000, 07, 07);
+     misEmpleados[1]=new Empleado("tatiana", 50000, 1995, 06, 15);
+     misEmpleados[2]=JefeRecursosHumanos;//Polimorfismo en accion
+     misEmpleados[3]=new Jefe("thiago", 95000, 1999, 5, 26);
+     
+    
+     
+    for(Empleado e: misEmpleados){
+       e.setsubeSueldo(5);
+       }         
+    
+    for(int i=0;i<misEmpleados.length;i++){
+     System.out.println("Nombre: "+misEmpleados[i].getdameNombre()+" sueldo: "+misEmpleados[i].getdameSueldo()+" Fecha de alta:"+misEmpleados[i].getdameFechaContrato());    
+    }
+    
+    
+    }
+}
